b7ae452f22bc7f86e08ba1192d7740f5066f7cb5
Gregory Olive <gregoryolive@outlook.com>
2022-02-08 01:03:22 +0900

    Add functions to display books in html

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-let myLibrary = [];
+// ADD BOOK TO LIBRARY
+
+let myLibrary = [1];
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -11,6 +13,83 @@ function addBookToLibrary(title, author, pages, read) {
   let newBook = new Book(title, author, pages, read)
   myLibrary.push(newBook)
 }
+
+// DISPLAY BOOKS VIA CARDS
+
+myLibrary.forEach(displayBook)
+
+function displayBook() {
+  const anchor = document.querySelector("#anchor");
+
+  const col = document.createElement("div");
+  col.className = "col";
+
+  const card = document.createElement("div");
+  card.className = "card text-center border-info";
+
+  card.appendChild(buildHeader());
+  card.appendChild(buildBody());
+  card.appendChild(buildFooter());
+  col.appendChild(card);
+  anchor.appendChild(col);
+}
+
+function buildHeader() {
+  const cardHeader = document.createElement("div");
+  cardHeader.className = "card-header";
+
+  const headerIcon = document.createElement("i");
+  headerIcon.className = "bi bi-check-lg text-info";
+
+  const headerText = document.createElement("span");
+  headerText.innerText = " Read";
+
+  cardHeader.appendChild(headerIcon);
+  cardHeader.appendChild(headerText);
+
+  return cardHeader;
+}
+
+function buildBody() {
+  const body = document.createElement("div");
+  body.className = "card-body";
+
+  const title = document.createElement("h3");
+  title.innerText = "The Hobbit";
+  body.appendChild(title);
+
+  const author = document.createElement("p");
+  author.innerText = "J. R. R. Tolkien";
+  body.appendChild(author);
+
+  return buildButtons(body);
+}
+
+function buildButtons(body) {
+  const readButton = document.createElement("a");
+  readButton.href = "#";
+  readButton.className = "btn btn-dark card-link";
+  readButton.innerText = "Mark Unread";
+  body.appendChild(readButton);
+
+  const deleteButton = document.createElement("a");
+  deleteButton.href = "#";
+  deleteButton.className = "btn btn-danger card-link";
+  deleteButton.innerText = "Delete Book";
+  body.appendChild(deleteButton);
+
+  return body
+}
+
+function buildFooter() {
+  const cardFooter = document.createElement("div");
+  cardFooter.className = "card-footer text-muted";
+  cardFooter.innerText = "340 pages"
+
+  return cardFooter
+}
+
+// OPEN AND CLOSE MODAL
 
 let modal = document.querySelector("#bookModal");
 
