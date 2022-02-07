09860126c71bb2696837c13195a721181c49f40e
Gregory Olive <gregoryolive@outlook.com>
2022-02-07 21:31:02 +0900

    Add functions for opening and closing modal

--- js/script.js
+++ js/script.js
@@ -11,3 +11,19 @@ function addBookToLibrary(title, author, pages, read) {
   let newBook = new Book(title, author, pages, read)
   myLibrary.push(newBook)
 }
+
+let modal = document.querySelector("#bookModal");
+
+function openModal() {
+  modal.style.display = "flex";
+}
+
+function closeModal() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
